216f8a900a740b8f28744d955aeb77bbcd49f16b
John Sanda <jsanda@redhat.com>
2015-08-28 11:02:13 -0400

    Cannot use InetAddress.getLoopbackAddress() since we have to support Java 6
    We default to 127.0.0.1 and allow it to be overridden via the
    java.rmi.server.hostname system property.

--- src/java/org/apache/cassandra/utils/RMIServerSocketFactoryImpl.java
+++ src/java/org/apache/cassandra/utils/RMIServerSocketFactoryImpl.java
@@ -1,8 +1,10 @@
 package org.apache.cassandra.utils;
 
 import java.io.IOException;
-import java.net.*;
+import java.net.InetAddress;
+import java.net.ServerSocket;
 import java.rmi.server.RMIServerSocketFactory;
+
 import javax.net.ServerSocketFactory;
 
 
@@ -10,7 +12,8 @@ public class RMIServerSocketFactoryImpl implements RMIServerSocketFactory
 {
 
     public ServerSocket createServerSocket(final int pPort) throws IOException  {
-        return ServerSocketFactory.getDefault().createServerSocket(pPort, 0, InetAddress.getLoopbackAddress());
+        String hostname = System.getProperty("java.rmi.server.hostname", "127.0.0.1");
+        return ServerSocketFactory.getDefault().createServerSocket(pPort, 0, InetAddress.getByName(hostname));
     }
 
     public boolean equals(Object obj)
